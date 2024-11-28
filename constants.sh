9a07bdb29df83034418cb8bd91ecb380405c1e3a
adamanteye <yangzheh22@gmail.com>
2024-11-28 08:35:17 +0800

    chore: regex typo

--- constants.sh
+++ constants.sh
@@ -3,4 +3,4 @@
 export AUTH4_LOG_URL="https://auth4.tsinghua.edu.cn/cgi-bin/srun_portal"
 export AUTH4_CHALLENGE_URL="https://auth4.tsinghua.edu.cn/cgi-bin/get_challenge"
 export REDIRECT_URI="http://www.tsinghua.edu.cn/"
-export REGEX_AC_ID='location\.href="http://auth[46].tsinghua.edu.cn/index_([0-9]+)\.html'
+export REGEX_AC_ID='location\.href="http://auth[46]\.tsinghua\.edu\.cn/index_([0-9]+)\.html'
