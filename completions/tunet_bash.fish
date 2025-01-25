a2757782ebe3f5dfc973fd4dd658da4e972d0893
adamanteye <ada@adamanteye.cc>
2025-01-25 23:28:13 +0800

    docs: fish completions

---
+++ completions/tunet_bash.fish
@@ -0,0 +1,4 @@
+complete -c tunet_bash -l config -d 'Specify username and password for account in use'
+complete -c tunet_bash -l login -d 'Make login request'
+complete -c tunet_bash -l logout -d 'Make logout request'
+complete -c tunet_bash -l whoami -d 'Query account username currently online'
